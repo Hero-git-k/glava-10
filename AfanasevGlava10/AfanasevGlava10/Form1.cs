74bd4374a36f5c7df45e35f64cf3ef2cb0e45a25
Denis <82368652+Hero-git-k@users.noreply.github.com>
2022-01-13 16:43:36 +0300

    Add files via upload

---
+++ AfanasevGlava10/AfanasevGlava10/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AfanasevGlava10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] astrLogicalDrives = System.IO.Directory.GetLogicalDrives();
+            foreach (string disk in astrLogicalDrives)
+                listBox1.Items.Add(disk);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] astrLogicalDrives = System.Environment.GetLogicalDrives();
+            foreach (string disk in astrLogicalDrives)
+                listBox1.Items.Add(disk);
+        }
+    }
+}
